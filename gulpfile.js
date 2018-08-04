f4b79e050417d98d94c2f4eb5f61d17c36f2add8
Stijn Geens <stijn.geens@outlook.be>
2018-08-04 12:01:47 +0200

    Minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 'use strict';
  
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
  
-gulp.task('sass', function () {
+gulp.task('style', function () {
   return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./css'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
+gulp.task('style:watch', function () {
+  gulp.watch('./scss/**/*.scss', ['style']);
 });
