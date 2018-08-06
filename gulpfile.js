094d3ef6a42aeddfbc6207ec439a99fb626c1428
Stijn Geens <stijn.geens@outlook.be>
2018-08-06 23:40:03 +0200

    Improve gulpfile (separate paths into objects)

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,21 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+
+var paths = {
+  styles: {
+    src: './scss/**/*.scss',
+    dest: './css'
+  }
+};
  
 gulp.task('style', function () {
-  return gulp.src('./scss/**/*.scss')
+  return gulp.src(paths.styles.src)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest(paths.styles.dest));
 });
  
 gulp.task('style:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['style']);
+  gulp.watch(paths.styles.src, ['style']);
 });
