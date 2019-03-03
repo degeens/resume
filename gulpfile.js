cfca74576aae48dfb12cc64c4829abec19ffed2b
degeens <stijn.geens@outlook.be>
2019-03-03 13:13:22 +0100

    Add Browsersync and default task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,30 @@
-'use strict';
- 
 var gulp = require('gulp');
-var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
 var paths = {
+  html: './**/*.html',
+  server: './',
   styles: {
     src: './scss/**/*.scss',
     dest: './css'
   }
 };
+
+gulp.task('serve', function() {
+  browserSync.init({ server: paths.server });
+
+  gulp.watch(paths.styles.src, gulp.series('styles'));
+  gulp.watch(paths.html).on('change', browserSync.reload);
+});
  
-gulp.task('style', function () {
+gulp.task('styles', function() {
   return gulp.src(paths.styles.src)
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest(paths.styles.dest));
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
 });
- 
-gulp.task('style:watch', function () {
-  gulp.watch(paths.styles.src, gulp.series('style'));
-});
+
+gulp.task('default', gulp.series('serve'));
