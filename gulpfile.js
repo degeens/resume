d798443dcfcbd3e2a636f3f6fcd7f185397e1db1
Stijn Geens <33414262+degeens@users.noreply.github.com>
2024-09-12 10:39:23 +0200

    Add English and Dutch translations

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,104 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass')(require('sass'));
-var rename = require('gulp-rename');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const replace = require('gulp-replace');
+const fs = require('fs');
+const path = require('path');
+const merge = require('merge-stream');
+const file = require('gulp-file');
 
 var paths = {
-  html: './**/*.html',
-  server: './',
-  styles: {
-    src: './scss/**/*.scss',
-    dest: './css'
-  }
+  html: {
+    src: './src/**/*.html',
+    dest: './dist/lang'
+  },
+  img: {
+    src: './src/assets/img/**/*',
+    dest: './dist/assets/img'
+  },
+  sass: {
+    src: './src/assets/scss/**/*.scss',
+    dest: './dist/assets/css'
+  },
+  translations: './src/translations/**/*.json'
 };
 
-gulp.task('serve', function() {
-  browserSync.init({ server: paths.server });
+gulp.task('translate', function() {
+  const mergedStream = merge();
 
-  gulp.watch(paths.styles.src, gulp.series('styles'));
-  gulp.watch(paths.html).on('change', browserSync.reload);
+  fs
+    .readdirSync('src/translations')
+    .forEach(translationFile => {
+      const language = path.basename(translationFile, '.json');
+      const translations = JSON.parse(fs.readFileSync(`src/translations/${translationFile}`));
+
+      let htmlStream = gulp.src(paths.html.src);
+
+      Object
+        .keys(translations)
+        .forEach(key => {
+          htmlStream = htmlStream.pipe(replace(`{{${key}}}`, translations[key]));
+        });
+
+      htmlStream = htmlStream.pipe(gulp.dest(`${paths.html.dest}/${language}`))
+
+      mergedStream.add(htmlStream);
+  });
+
+  return mergedStream;
 });
+
+gulp.task('create-redirect', function () {
+  const redirectHtml = 
+  `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirecting...</title>
+    <script>
+      // Detect browser language
+      const userLang = navigator.language || navigator.userLanguage;
+      const lang = userLang.split('-')[0]; // Get language code (e.g., 'en' from 'en-US')
+
+      // Define supported languages
+      const supportedLanguages = ['en', 'nl'];
+
+      // Default to 'en' if language is not supported
+      if (!supportedLanguages.includes(lang)) {
+        window.location.href = './lang/en/index.html';
+      } else {
+        // Redirect to the appropriate language directory
+        window.location.href = './lang/' + lang + '/index.html';
+      }
+    </script>
+  </head>
+  <body>
+    <p>Redirecting you to the correct language version...</p>
+  </body>
+  </html>`;
+
+  return file('index.html', redirectHtml, { src: true })
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy-images', function() {
+  return gulp.src(paths.img.src, { encoding: false })
+  .pipe(gulp.dest(paths.img.dest));
+})
  
-gulp.task('styles', function() {
-  return gulp.src(paths.styles.src)
+gulp.task('sass', function() {
+  return gulp.src(paths.sass.src)
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.styles.dest))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest(paths.sass.dest));
 });
 
-gulp.task('default', gulp.series('serve'));
+gulp.task('watch', function() {
+  gulp.watch(paths.html.src, gulp.series('translate'));
+  gulp.watch(paths.translations, gulp.series('translate'));
+  gulp.watch(paths.img.src, gulp.series('copy-images'));
+  gulp.watch(paths.sass.src, gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series('translate', 'create-redirect', 'copy-images', 'sass', 'watch'));
